8f1d397dcbc77e477f8511418155ead4f3ed9dc4
Juan David Castro Marín <jcastromarin@gmail.com>
2023-10-23 11:10:47 -0500

    Agregar archivos de proyecto.

---
+++ FabricaComputadoras.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Actidad1;
+
+public class FabricaComputadoras 
+{         
+       public  FabricaComputadoras()
+	   {
+	   	Console.WriteLine("***************************************");
+		   Console.WriteLine("Bienvenido a la fabrica de computadores");
+		   Console.WriteLine("***************************************\n");
+		   
+		   
+		   Console.WriteLine("Ingrese el número de computadoras a comprar");
+		   if(!int.TryParse(Console.ReadLine(), out int cantidadComputadores))
+		   {
+		   	cantidadComputadores= 0;
+		   }
+		   
+		   //Se valida la cantidad ingresada
+		   if (cantidadComputadores == 0){
+		   	Console.WriteLine("La cantidad ingresada de computadores no es válido. ");
+			   return;
+		  
+		   }
+		   
+		   //Se establece elnvalor de los computadores
+		   const double valorComputador = 11000;
+		   
+		   //Descuento del 10
+	       if(cantidadComputadores < 5){
+		   		Console.WriteLine($"El valor total para {cantidadComputadores} es de {valorComputador - (valorComputador * 0.10)}");
+		   }
+		   
+		   //Descuento del 20
+		   if(cantidadComputadores >= 5 || cantidadComputadores < 10){
+		   		Console.WriteLine($"El valor total para {cantidadComputadores} es de {valorComputador - (valorComputador * 0.20)}");
+		   }
+		   
+		   //Descuento del 40
+		   if(cantidadComputadores > 10){
+		   		Console.WriteLine($"El valor total para {cantidadComputadores} es de {valorComputador - (valorComputador * 0.40)}");
+		   }
+		  
+	   }
+}
+
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Actidad1;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Menu();
+    }
+
+    public static void Menu()
+    {
+        Console.WriteLine("***********************************");
+        Console.WriteLine("Bienvenido a  la actividad numero 1");
+        Console.WriteLine("***********************************");
+
+        int opcion = 0;
+
+        while (opcion == 0)
+        {
+
+            Console.WriteLine("1. Fabrica.de computadoras ");
+            Console.WriteLine("2. Llantera.");
+            Console.WriteLine("3. Juego de preguntas.");
+            Console.WriteLine("4. Proveedor de equipos de sonidos.");
+            Console.WriteLine("5. Fruteria.");
+            Console.WriteLine("6. Numero medio.");
+            Console.WriteLine("Ingrese el numero del ejercicio que desea ejecutar o presione 0 para salir");
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    //Se limpia la pantallaxaa
+                    Console.Clear();
+                    //Se inicia el ejercicio uno
+                    FabricaComputadoras fabricaComputadoras = new FabricaComputadoras();
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                default:
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                    break;
+
+
+            }
+
+
+
+        }
+
+    }
+
+}
